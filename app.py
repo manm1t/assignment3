b637cdde58c64af97c4a68b553c1ee383d03bff2
manm1t <manmitsingh.sandhu@gmail.com>
2025-12-09 06:55:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello from sandhu ECS Container!"
+    return "Hello from Sandhu ECS Container!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=80)
